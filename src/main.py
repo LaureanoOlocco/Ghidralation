10c33ac6f876a96284a7fcaee8711c2f15d0e12b
LaureanoOlocco <laureanoolocco@gmail.com>
2025-08-07 11:23:56 -0300

    Fix main.py for search plugins in plugins/

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 # @category GCL
 # @runtime PyGhidra
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         import importlib.util
         import sys
         import os
         import cp
+
         print(f"Python version: {sys.version}")
     except:
         sys.path.append(os.path.dirname(__file__))
@@ -14,9 +15,14 @@ if __name__ == '__main__':
     finally:
         plugin = "findcrypt3"
         cp.currentProgram = currentProgram
-        path = os.path.dirname(__file__)+'/'+plugin+'.py'
-        print(f"Loading plugin from: {path}")
-        spec = importlib.util.spec_from_file_location(plugin,path)
+
+        plugin_dir = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), "..", "plugins")
+        )
+        plugin_path = os.path.join(plugin_dir, plugin + ".py")
+
+        print(f"Loading plugin from: {plugin_path}")
+        spec = importlib.util.spec_from_file_location(plugin, plugin_path)
         module = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(module)
         module.PLUGIN_ENTRY().run(0)
