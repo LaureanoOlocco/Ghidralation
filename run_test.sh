9b009305982a224249390e0e98bd6aff75fe6fa7
LaureanoOlocco <laureano.olocco@eclypsium.com>
2025-08-25 20:16:50 -0300

    Fix run_test.sh
    Signed-off-by: LaureanoOlocco <laureano.olocco@eclypsium.com>

--- run_test.sh
+++ run_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-GHIDRA_SUPPORT="/home/aurean/ghidra/ghidra_11.4_PUBLIC/support"
-PROJECT_DIR="/home/aurean/GhidraProjects"
+GHIDRA_SUPPORT="/home/eclypsium/Documents/ghidra/ghidra_11.4.1_PUBLIC/support"
+PROJECT_DIR="/home/eclypsium/Documents/GhidraProjects"
 PROJECT_NAME="syms2elf"
 BINARY_NAME="crypto_binary"
-SCRIPT_PATH="/home/aurean/Desktop/Ghidralation/test/test_suite.py"
+SCRIPT_PATH="/home/eclypsium/Documents/Ghidralation/test/test_suite.py"
 
 "$GHIDRA_SUPPORT/pyghidraRun" --headless "$PROJECT_DIR" "$PROJECT_NAME" \
   -process "$BINARY_NAME" \
